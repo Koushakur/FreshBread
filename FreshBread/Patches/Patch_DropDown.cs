32544fd50bc865ad09cca67593a1159986f9882f
Duker <m.oliver.henriksson@gmail.com>
2026-06-24 02:16:38 +0200

    Make both GBGetter and GBSetter dropdowns stay within the window

---
+++ FreshBread/Patches/Patch_DropDown.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using BrilliantSkies.Ui.Consoles.Interpretters.Simple;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+
+namespace FreshBread.Patches {
+
+    [HarmonyPatch(typeof(DropDown<object, object>), nameof(DropDown<object, object>.Draw))]
+    public class Patch_DropDown_GenericBlockGetter {
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+
+            foreach (var instruction in instructions) {
+
+                if (instruction.opcode == OpCodes.Ldc_R4
+                    && instruction.operand is float f
+                    && f == 20f) {
+
+                    yield return new CodeInstruction(OpCodes.Ldc_R4, 35f);
+
+                } else
+                    yield return instruction;
+            }
+        }
+    }
+}
---
+++ FreshBread/Patches/Patch_GenericBlockSetter.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using BrilliantSkies.Blocks.BreadBoards;
+using BrilliantSkies.Ui.Consoles.Interpretters.Simple;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using static BrilliantSkies.Blocks.BreadBoards.GenericBlockSetter;
+
+
+namespace FreshBread.Patches {
+
+    [HarmonyPatch(typeof(GenericBlockSetter), nameof(GenericBlockSetter.PopulateSegment))]
+    public class Patch_GenericBlockSetter {
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+
+            var codes = instructions.ToList();
+            var outCodes = new List<CodeInstruction>();
+
+            for (int i = 0; i < codes.Count; i++) {
+
+                outCodes.Add(codes[i]);
+
+                if (codes[i].opcode == OpCodes.Newobj
+                    && codes[i].operand is ConstructorInfo ci
+                    && ci.DeclaringType != null
+                    && ci.DeclaringType.IsGenericType
+                    && ci.DeclaringType == typeof(DropDown<GenericBlockSetter, ModuleAttributePair>)) {
+
+                    MethodInfo setter = AccessTools.PropertySetter(ci.DeclaringType, "LimitToMasterRectWidth");
+
+                    outCodes.Add(new CodeInstruction(OpCodes.Dup));
+                    outCodes.Add(new CodeInstruction(OpCodes.Ldc_I4_1));
+                    outCodes.Add(new CodeInstruction(OpCodes.Callvirt, setter));
+                }
+            }
+
+            return outCodes;
+        }
+
+    }
+}
